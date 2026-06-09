ded59ce5dae7d3a7ac64879ecc28f7cb62c664c6
thuongtt2004 <dubu2k4@gmail.com>
2026-06-09 13:45:52 +0700

    Sửa múi giờ: Đặt timezone Việt Nam (GMT+7) cho PHP và MySQL

--- connect.php
+++ connect.php
@@ -1,4 +1,7 @@
 <?php
+// Đặt múi giờ Việt Nam
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -11,5 +14,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
+
+// Đặt timezone cho MySQL
+$conn->query("SET time_zone = '+07:00'");
 // echo "Kết nối CSDL '$dbname' thành công!";
 ?>
